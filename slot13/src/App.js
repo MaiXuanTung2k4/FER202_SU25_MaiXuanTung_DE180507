21bebc71c79b4c59d6a43aa9096f65dec557d7f9
MaiXuanTung2k4 <maixuantung4job@gmail.com>
2025-10-24 10:59:44 +0700

    updateUseContextSlot14

--- slot13/src/App.js
+++ slot13/src/App.js
@@ -1,16 +1,27 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { ThemeProvider } from "./contexts/ThemeContext";
-import CounterComponent from "./components/CounterComponent";
-import LightSwitch from "./components/LightSwitch";
+import React from 'react';
+import { ThemeProvider } from './contexts/ThemeContext';
+import LightSwitch from './components/LightSwitch'; // nếu có sẵn trong project
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CounterComponent from './components/CounterComponent'; // nếu có sẵn
+import { AuthProvider } from './contexts/AuthContext';
+import LoginForm from './components/LoginForm';
 
-export default function App() {
+function App() {
   return (
-    <ThemeProvider>
-      <div style={{ minHeight: "100vh", transition: "all 0.3s ease" }}>
+    <div>
+      <ThemeProvider>
+        
         <CounterComponent />
         <LightSwitch />
-      </div>
-    </ThemeProvider>
+      </ThemeProvider>
+
+      <AuthProvider>
+        <div className="App">
+          <LoginForm />
+        </div>
+      </AuthProvider>
+    </div>
   );
 }
+
+export default App;
