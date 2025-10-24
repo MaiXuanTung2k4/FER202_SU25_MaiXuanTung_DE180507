1f4d0baa430d4c86a9a71d06d35f603f5dc6842c
MaiXuanTung2k4 <maixuantung4job@gmail.com>
2025-10-24 11:27:56 +0700

    slot14

---
+++ slot14/src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <div>
+      <h2>Trang Liên Hệ</h2>
+      <p>Liên hệ với chúng tôi qua email: contact@example.com</p>
+    </div>
+  );
+}
